1abe5a9f3600ea3a1a1723ba4c31959c7ab862b8
dsxbb <13192872690@163.com>
2018-07-12 11:21:54 +0800

    add 1.8

---
+++ chapter1/1.8.scm
@@ -0,0 +1,26 @@
+(define (cube-root x)
+  (cube-root-iter 1.0 x))
+
+(define (cube-root-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cube-root-iter (improve guess x)
+                      x)))
+
+(define (improve guess x)
+  (/ (+ (/ x (square guess)) (* 2 guess))
+     3))
+
+
+(define (good-enough? guess x)
+  ( < (abs (- (cube guess) x))
+      0.001))
+
+(define (squre x)
+  (* x x ))
+
+(define (cube x)
+  (* x x x))
+
+
+(cube-root(* 3 3 3))
