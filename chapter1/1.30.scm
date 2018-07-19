f4662b6527004771b29494ce441fcaee4e3d680c
dsxbb <13192872690@163.com>
2018-07-19 12:26:17 +0800

    add 1.30

---
+++ chapter1/1.30.scm
@@ -0,0 +1,12 @@
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ (term a) result))))
+  (iter a 0))
+
+(define (inc n)
+  (+ n 1))
+
+
+(sum inc 1 inc 5)
