f192887be14105a359b6bcfbf34b701edd2558d4
dsxbb <13192872690@163.com>
2018-08-09 12:19:00 +0800

    add 2.38

---
+++ chapter2/2.38-fold-left.scm
@@ -0,0 +1,15 @@
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest)
+        result
+        (iter (op result (car rest))
+              (cdr rest))))
+  (iter initial sequence))
+
+(fold-left / 1 (list 1 2 3))
+
+(fold-right / 1 (list 1 2 3))
+
+(fold-left list '() (list 1 2 3))
+
+(fold-right list '() (list 1 2 3))
